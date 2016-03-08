e31b34d2cae6b2f7020971b872877e784bb32192
fisheyz <tiago_m4t3us@hotmail.com>
2016-03-08 19:04:54 +0000

    def final

--- spiral.java
+++ spiral.java
@@ -7,24 +7,15 @@ class spiral{
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
 
-    int count=1;
-    int x=0;
-    int y=0;
-    int k=0;
-
+    int count=1, x=0, y=0, k=0;
 
     while(count<n ){
       k++;
       for(int i=0; i < k && count <n;  count++, i++){x++;}
-
-
       for(int i=0; i<k && count <n; count++, i++){y--;}
-
       k++;
       for(int i=0; i<k && count <n;  count++, i++){x--;}
-
       for(int i=0; i<k && count <n; count++, i++){y++;}
-
     }
     System.out.println("("+x+ "," +y+")");
   }
