8e8b0c99d765621eb74f4d3bd45bfb2d505c41a0
fisheyz <tiago_m4t3us@hotmail.com>
2016-03-10 10:10:33 +0000

    funciona

---
+++ los.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class los{
+  public static void main(String[] args){
+
+    Scanner in = new Scanner(System.in);
+    int k = in.nextInt();
+    int m = k, n = k, j=1;
+
+    while (k-1 >= 0){
+
+
+      for (int i = 0; i<n;i++){
+        if (  i >= n/2+j || i <= n/2-j)
+          System.out.print(".");
+        else
+          System.out.print("#");
+        }
+        System.out.println();
+
+        if (k <= m/2+1)
+        j--;
+        else
+        j++;
+      
+        k--;
+    }
+  }
+}
