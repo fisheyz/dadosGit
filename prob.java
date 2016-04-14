e987970ee92db1a32d0509417f54f98cdd678568
fisheyz <tiago_m4t3us@hotmail.com>
2016-04-14 18:36:37 +0100

    scanner

--- prob.java
+++ prob.java
@@ -5,25 +5,36 @@ class prob{
 
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    for(int i = 0; i<n;i++)
-    System.out.println(npi(in));
-
+    for(int i = 0; i<n;i++){
+	if (npi(in) == true)
+	    System.out.println(npi.s);
+	else
+	    System.out.println("Expressao Incorrecta");
+    }
   }
 
-  public static Stack npi(Scanner in){
+  public static boolean npi(Scanner in){
     char op;
     Integer x,y;
     Stack<Integer> s = new Stack<Integer>();
-    if (in.hasNextInt())
-    s.push(new Integer(in.nextInt()));
-    else{
-      op = in.next().charAt(0);
-      x = s.pop();
-      y = s.pop();
-      s.push(new Integer(contas(x,y,op)));
+    while (in.hasNext() && s.size() >1){
+	
+	if (in.hasNextInt())
+	    s.push(new Integer(in.nextInt()));
+	else{
+	    
+	    op = in.next().charAt(0);
+	    
+	    if(s.size() <2 || !in.hasNextInt())
+		return false;
+	    
+	    x = s.pop();
+	    y = s.pop();
+	    s.push(new Integer(contas(x,y,op)));
+	}
     }
-    return s;
-
+    return true;
+ 
   }
   public static int contas(int x, int y, char op){
     if (op == '+')
